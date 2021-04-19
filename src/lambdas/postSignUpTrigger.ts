4646cd99e7c9e4bb13abe26cfc8465089887baf4
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-04-19 10:46:27 +0200

    Refactor

--- src/lambdas/postSignUpTrigger.ts
+++ src/lambdas/postSignUpTrigger.ts
@@ -2,29 +2,22 @@ import { PostConfirmationConfirmSignUpTriggerEvent } from 'aws-lambda';
 import { CognitoIdentityServiceProvider } from 'aws-sdk';
 
 const postSignUpTrigger = async (
-  event: PostConfirmationConfirmSignUpTriggerEvent
-): Promise<string> => {
-  return new Promise(async (resolve, reject) => {
-    const cognitoISP = new CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
-    const { userName, userPoolId } = event;
+  event: PostConfirmationConfirmSignUpTriggerEvent,
+): Promise<string> => new Promise((resolve, reject) => {
+  const cognitoISP = new CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
+  const { userName, userPoolId } = event;
 
-    const userGroupParams = {
-      GroupName: 'buyers',
-      UserPoolId: userPoolId,
-      Username: userName,
-    };
+  const userGroupParams = {
+    GroupName: 'buyers',
+    UserPoolId: userPoolId,
+    Username: userName,
+  };
 
-    try {
-      await cognitoISP
-        .adminAddUserToGroup(userGroupParams)
-        .promise();
-
-      // if all is good return the username else return the error from the cognitoISP.promise()
-      resolve(userName);
-    } catch (error) {
-      reject(error);
-    }    
-  });
-};
+  cognitoISP
+    .adminAddUserToGroup(userGroupParams)
+    .promise()
+    .then(() => resolve(userName))
+    .catch((error) => reject(error));
+});
 
 export default postSignUpTrigger;
